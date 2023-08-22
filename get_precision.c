a7c81a98ac509c15d0dd62638828e208519ee970
mosesomondi-1 <130564767+mosesomondi-1@users.noreply.github.com>
2023-08-22 23:20:35 +0300

    Update get_precision.c

--- get_precision.c
+++ get_precision.c
@@ -2,10 +2,10 @@
 
 /**
  * get_precision - Calculates the precision for printing
+ * By Joshua and Moses
  * @format: Formatted string in which to print the arguments
  * @i: List of arguments to be printed.
  * @list: list of arguments.
- *
  * Return: Precision.
  */
 int get_precision(const char *format, int *i, va_list list)
